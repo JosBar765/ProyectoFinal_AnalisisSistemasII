7aa76aa6bc709f542862b3c94ddb573f17317944
Josué Barrios Ramírez <130526195+JosBar765@users.noreply.github.com>
2026-08-10 16:35:03 -0600

    raw spring

---
+++ src/main/java/com/josbar/medisistemas/MedisistemasApplication.java
@@ -0,0 +1,13 @@
+package com.josbar.medisistemas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedisistemasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedisistemasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josbar/medisistemas/MedisistemasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josbar.medisistemas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedisistemasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
