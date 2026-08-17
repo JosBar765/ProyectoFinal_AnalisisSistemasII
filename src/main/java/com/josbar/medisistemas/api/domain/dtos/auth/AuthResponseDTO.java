471d15f711d3aae0de6b5f9ec2f6ad4a11438927
Josué Barrios Ramírez <130526195+JosBar765@users.noreply.github.com>
2026-08-16 20:21:14 -0600

    agregando dtos para el inicio de sesión

--- src/main/java/com/josbar/medisistemas/api/domain/dtos/auth/AuthResponseDTO.java
+++ src/main/java/com/josbar/medisistemas/api/domain/dtos/auth/AuthResponseDTO.java
@@ -1,4 +1,18 @@
 package com.josbar.medisistemas.api.domain.dtos.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class AuthResponseDTO {
+
+    private String token;
+
+    // Nota: El JWT ya contiene en su "payload" el idUsuario y posiblemente el Rol.
+    // Por tus reglas de diseño, Angular deberá decodificar el token para saber quién inició sesión,
 }
--- src/main/java/com/josbar/medisistemas/api/domain/dtos/auth/LoginRequestDTO.java
+++ src/main/java/com/josbar/medisistemas/api/domain/dtos/auth/LoginRequestDTO.java
@@ -1,4 +1,16 @@
 package com.josbar.medisistemas.api.domain.dtos.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class LoginRequestDTO {
+    private String correo;
+    private String contrasenia;
+
 }
